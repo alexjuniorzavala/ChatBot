76594e2e0212dd2def9974cb3a7633e3492e14cd
Alex Junior <alexjuniorzavalas@gmail.com>
2025-09-22 00:36:45 +0300

    Getting the qty of messages and getting them

--- main2.py
+++ main2.py
@@ -8,6 +8,8 @@ import time
 user_data = r"D:\Alex\Projetos\Python\Chatbot\ChromeProfile"
 profile_dir = "Default"
 
+path_driver = r"D:\Alex\Projetos\Python\Chatbot\chromedriver.exe"
+
 options = uc.ChromeOptions()
 options.add_argument(f"--user-data-dir={user_data}")
 options.add_argument(f"--profile-directory={profile_dir}")
@@ -16,27 +18,30 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = uc.Chrome(options=options, version_main=137)
+
+
+driver = uc.Chrome(driver_executable_path=path_driver, options=options)
 driver.get("https://web.whatsapp.com")
 
 print("Aguardando WhatsApp Web carregar...")
-wait = WebDriverWait(driver, 30)
+wait = WebDriverWait(driver, 5)
 
 # XPaths
 noti_xpath = "//div[@class='_ak8j']//div[@role='gridcell']//span//div[contains(@class,'_ahlk')]//span//span[contains(@class,'x140p0ai')]"
-msg_in_xpath = '//div[contains(@class,"message-in")]//span[@class="copyable-text"]//span'
-msg_out_xpath = '//div[contains(@class,"message-out")]//span[@class="copyable-text"]//span'
-
+msg_in_xpath = '//div[contains(@class,"message-in")]//span[@dir="ltr"]'
+msg_out_xpath = '//div[contains(@class,"message-out")]//span[@dir="ltr"]'
 
 # Espera até a barra lateral aparecer
-wait.until(EC.presence_of_element_located((By.ID, "side")))
+WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "side")))
 
 try:
     bolhas = driver.find_elements(By.XPATH, noti_xpath)
     print(f"Encontradas {len(bolhas)} bolinhas de notificação.")
 
     if bolhas:
-        print("Novas mensagens encontradas. Abrindo...")
+        for qty_msg in bolhas:
+            print(qty_msg.text)
+        
         bolhas[0].click()
         print("Abri a conversa.")
 
@@ -62,6 +67,5 @@ try:
 
 except Exception as e:
     print("Erro:", e)
-
 input("\nPressione Enter para fechar...")
 driver.quit()
